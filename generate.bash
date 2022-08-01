8f6b0689b61aec2dd6263733f54da15c05c3f5ab
Pranshu Srivastava <rexagod@gmail.com>
2022-08-01 16:39:46 +0530

    Generate configmaps.

--- generate.bash
+++ generate.bash
@@ -4,9 +4,8 @@ function generate {
   cd source/"$1"/
   for dashboard in *; do
     dashboard=${dashboard%%.*}
-    out=../configmaps/"$1"/${dashboard}.configmap.yaml
-    kubectl create configmap ${dashboard} \
-      --from-file=${dashboard}.json -o yaml --dry-run=client > ${out}
+    out=../../configmaps/"$1"/${dashboard}.configmap.yaml
+    kubectl create configmap ${dashboard} --from-file=${dashboard}.json -o yaml --dry-run=client > ${out}
 cat << EOF >> ${out}
   labels:
     grafana_dashboard: "true"
